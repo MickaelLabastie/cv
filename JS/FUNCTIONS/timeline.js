27fbdfe1dfd8ea897198781c58c68646e0a4879f
Mickaelux <mickaelux@gmail.com>
2021-12-07 11:42:44 +0100

    Mega update for timeline

--- JS/FUNCTIONS/timeline.js
+++ JS/FUNCTIONS/timeline.js
@@ -1,15 +1,21 @@
-var timelineItems = document.querySelectorAll('.timeline-item');
+var open_buttons = document.querySelectorAll('.openBtn');
+var close_buttons = document.querySelectorAll('.closeBtn');
 
-var details = function(){
-        var selector = this.id+'Details';
+var openDetails = function(){
+        var selector = this.parentElement.id+'Details';
         var targetElement = document.getElementById(selector);
-        targetElement.classList = 'details';
-        targetElement.addEventListener('click', function(){
-            targetElement.classList = 'none';
-        });
+        targetElement.classList = 'detailsOn';
 }
 
-for(let index = 0;index < timelineItems.length;index++){
-    var timelineItem = timelineItems[index];
-    timelineItem.addEventListener('click', details);
+var closeDetails = function(){
+    var selector = this.parentElement.id;
+    var targetElement = document.getElementById(selector);
+    targetElement.classList = 'detailsOff';
+}
+
+for(let index = 0;index < open_buttons.length;index++){
+    var open_button = open_buttons[index];
+    var close_button = close_buttons[index];
+    open_button.addEventListener('click', openDetails);
+    close_button.addEventListener('click', closeDetails);
 }
