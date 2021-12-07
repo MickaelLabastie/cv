44a1cb63f4b718abafda9fa81b28f58e943b0efd
Mickaelux <mickaelux@gmail.com>
2021-12-07 13:09:25 +0100

    Update of timeline JS function, index.html & style.css

--- JS/FUNCTIONS/timeline.js
+++ JS/FUNCTIONS/timeline.js
@@ -1,16 +1,26 @@
 var open_buttons = document.querySelectorAll('.openBtn');
 var close_buttons = document.querySelectorAll('.closeBtn');
+var timeline = document.getElementById('timeline');
+var button;
 
 var openDetails = function(){
-        var selector = this.parentElement.id+'Details';
-        var targetElement = document.getElementById(selector);
-        targetElement.classList = 'detailsOn';
+    var selector = this.parentElement.id+'Details';
+    var targetElement = document.getElementById(selector);
+    targetElement.classList = 'detailsOn';
+    for (let index = 0; index < open_buttons.length; index++) {
+        button = open_buttons[index];
+        button.style.pointerEvents = 'none';
+    }
 }
 
 var closeDetails = function(){
     var selector = this.parentElement.id;
     var targetElement = document.getElementById(selector);
     targetElement.classList = 'detailsOff';
+    for (let index = 0; index < open_buttons.length; index++) {
+        button = open_buttons[index];
+        button.style.pointerEvents = 'auto';
+    }
 }
 
 for(let index = 0;index < open_buttons.length;index++){
