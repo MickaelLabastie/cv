be95fec5773d8c3cd784885aadde6ddf51a71d3a
Mickaelux <mickaelux@gmail.com>
2021-12-08 16:29:34 +0100

    Update training.js file

---
+++ JS/FUNCTIONS/training.js
@@ -0,0 +1,12 @@
+var controller = new ScrollMagic.Controller();
+var trainingTween = new TweenMax.to('.training-item', 1, {
+    width: '50%',
+    border: '2px solid #FFF',
+    borderRadius: '10px',
+    backgroundColor: '#000'
+});
+var trainingScene = new ScrollMagic.Scene({
+    triggerElement: '.training',
+    triggerHook: 0.2
+})
+.setTween(trainingTween).addIndicators().addTo(controller);
