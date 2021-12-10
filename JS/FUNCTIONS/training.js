6bd2677d370676a2361c772ea2593aaff55d5938
Mickaelux <mickaelux@gmail.com>
2021-12-10 11:52:21 +0100

    Update

--- JS/FUNCTIONS/training.js
+++ JS/FUNCTIONS/training.js
@@ -1,12 +1,32 @@
 var controller = new ScrollMagic.Controller();
-var trainingTween = new TweenMax.to('#trngItem01', 0.1, {
-    width: '50%',
-    border: '2px solid #FFF',
-    borderRadius: '10px',
-    backgroundColor: '#000'
-});
-var trainingScene = new ScrollMagic.Scene({
-    triggerElement: '.training',
-    triggerHook: 0.2
+
+/*CF GSAP*/
+// var trainingTween = new TweenMax.to('#trngItem01', 0.6, {
+//     width: '50%',
+//     border: '2px solid #FFF',
+//     borderRadius: '10px',
+//     backgroundColor: '#000'
+// });
+
+var trainingScene01 = new ScrollMagic.Scene({
+    duration: 100,
+    triggerElement: '#trngItem01',
 })
-.setTween(trainingTween).addIndicators().addTo(controller);
+.setClassToggle('#trngItem01', 'change')
+.addIndicators()
+.addTo(controller);
+
+var trainingScene02 = new ScrollMagic.Scene({
+    duration: 100,
+    triggerElement: '#trngItem02',
+})
+.setClassToggle('#trngItem02', 'change')
+.addIndicators()
+.addTo(controller);
+
+trainingScene01.triggerHook(0.2);
+trainingScene02.triggerHook(0.2);
+
+/*.setTween(trainingTween)*/
+
+
