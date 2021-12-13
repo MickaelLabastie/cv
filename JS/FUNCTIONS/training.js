64e583ee87cab531062d13e4a6882e1bbe1e5fa2
Mickaelux <mickaelux@gmail.com>
2021-12-13 22:58:13 +0100

    Massive Update

--- JS/FUNCTIONS/training.js
+++ JS/FUNCTIONS/training.js
@@ -1,16 +1,24 @@
-var trngItem01 = document.getElementById('trngItem01');
-
 var controller = new ScrollMagic.Controller();
 
-for (let index = 1; index <= 6; index++) {
-    var trainingScene = new ScrollMagic.Scene({
-        duration: 100,
-        triggerElement:'#trngItem0'+index,
-    })
-    .setClassToggle('#trngItem0'+index, 'change')
-    // Add some indicators to visualize beginning and end of animation
-    .addIndicators()
-    .addTo(controller);
+var trainingScene = new ScrollMagic.Scene({
+    triggerElement:'#training',
+    triggerHook: 0.18,
+})
+.setClassToggle('.training-item', 'change')
+// Add some indicators to visualize beginning and end of animation
+// .addIndicators()
+.addTo(controller);
 
-    trainingScene.triggerHook(0.24);
-}
+//Example for multiple scenes on multiple elements
+// for (let index = 1; index <= 6; index++) {
+//     var trainingScene = new ScrollMagic.Scene({
+//         duration: '15%',
+//         triggerElement:'#trngItem0'+index,
+//     })
+//     .setClassToggle('#trngItem0'+index, 'change')
+//     // Add some indicators to visualize beginning and end of animation
+//     .addIndicators()
+//     .addTo(controller);
+
+//     trainingScene.triggerHook(0.24);
+// }
