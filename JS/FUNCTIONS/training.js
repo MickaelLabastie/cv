0f4d2a636f165928c819cefdb31b086d7d1274b7
Mickaelux <mickaelux@gmail.com>
2021-12-13 16:28:43 +0100

    Update

--- JS/FUNCTIONS/training.js
+++ JS/FUNCTIONS/training.js
@@ -1,15 +1,16 @@
+var trngItem01 = document.getElementById('trngItem01');
+
 var controller = new ScrollMagic.Controller();
 
 for (let index = 1; index <= 6; index++) {
     var trainingScene = new ScrollMagic.Scene({
-        duration: 95,
+        duration: 100,
         triggerElement:'#trngItem0'+index,
     })
     .setClassToggle('#trngItem0'+index, 'change')
+    // Add some indicators to visualize beginning and end of animation
     .addIndicators()
     .addTo(controller);
 
-    trainingScene.triggerHook(0.2);
+    trainingScene.triggerHook(0.24);
 }
-
-
