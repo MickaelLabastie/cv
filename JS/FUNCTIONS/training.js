82a46425eb9c4c71d02240b96c7f19072bcb8a44
Mickaelux <mickaelux@gmail.com>
2021-12-13 09:32:49 +0100

    Definitive update of training.js function

--- JS/FUNCTIONS/training.js
+++ JS/FUNCTIONS/training.js
@@ -1,32 +1,15 @@
 var controller = new ScrollMagic.Controller();
 
-/*CF GSAP*/
-// var trainingTween = new TweenMax.to('#trngItem01', 0.6, {
-//     width: '50%',
-//     border: '2px solid #FFF',
-//     borderRadius: '10px',
-//     backgroundColor: '#000'
-// });
+for (let index = 1; index <= 6; index++) {
+    var trainingScene = new ScrollMagic.Scene({
+        duration: 95,
+        triggerElement:'#trngItem0'+index,
+    })
+    .setClassToggle('#trngItem0'+index, 'change')
+    .addIndicators()
+    .addTo(controller);
 
-var trainingScene01 = new ScrollMagic.Scene({
-    duration: 100,
-    triggerElement: '#trngItem01',
-})
-.setClassToggle('#trngItem01', 'change')
-.addIndicators()
-.addTo(controller);
-
-var trainingScene02 = new ScrollMagic.Scene({
-    duration: 100,
-    triggerElement: '#trngItem02',
-})
-.setClassToggle('#trngItem02', 'change')
-.addIndicators()
-.addTo(controller);
-
-trainingScene01.triggerHook(0.2);
-trainingScene02.triggerHook(0.2);
-
-/*.setTween(trainingTween)*/
+    trainingScene.triggerHook(0.2);
+}
 
 
