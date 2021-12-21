9450b329828ae77a03cb5245313aacc23309cbc4
Mickaelux <mickaelux@gmail.com>
2021-12-21 21:54:24 +0100

    Update media querries part two online tests

--- JS/FUNCTIONS/burger.js
+++ JS/FUNCTIONS/burger.js
@@ -1,10 +1,16 @@
 var burger = document.querySelector('.burger');
 var bar = document.querySelector('.bar');
 var nav = document.getElementById('navigation');
+var navLinks = document.getElementsByClassName('navLink');
 
-burger.addEventListener('click', () => {
-
+var pop = () => {
     bar.classList.toggle('change');
     nav.classList.toggle('pop');
+}
 
-});
+burger.addEventListener('click', pop);
+    
+for (let index = 0; index < navLinks.length; index++) {
+    let element = navLinks[index];
+    element.addEventListener('click', pop);
+}
