de1eb2d889fa715f8c0f07019e7029427ae40ffc
Mickaelux <mickaelux@gmail.com>
2022-01-03 23:10:15 +0100

    Update corrections of article#pro, index+style+js

--- JS/FUNCTIONS/timeline.js
+++ JS/FUNCTIONS/timeline.js
@@ -2,7 +2,6 @@
 
 var open_buttons = document.querySelectorAll('.openBtn');
 var close_buttons = document.querySelectorAll('.closeBtn');
-var timeline = document.getElementById('timeline');
 var button;
 
 
@@ -10,6 +9,7 @@ var openDetails = function(){
     var selector = this.parentElement.id+'Details';
     var targetElement = document.getElementById(selector);
     targetElement.classList = 'detailsOn';
+    targetElement.nextElementSibling.style.display = 'flex';
     for (let index = 0; index < open_buttons.length; index++) {
         button = open_buttons[index];
         button.style.pointerEvents = 'none';
@@ -17,13 +17,14 @@ var openDetails = function(){
 }
 
 var closeDetails = function(){
-    var selector = this.parentElement.id;
+    var selector = this.previousElementSibling.id;
     var targetElement = document.getElementById(selector);
     targetElement.classList = 'detailsOff';
     for (let index = 0; index < open_buttons.length; index++) {
         button = open_buttons[index];
         button.style.pointerEvents = 'auto';
     }
+    this.style.display = 'none';
 }
 
 for(let index = 0;index < open_buttons.length;index++){
@@ -32,3 +33,4 @@ for(let index = 0;index < open_buttons.length;index++){
     open_button.addEventListener('click', openDetails);
     close_button.addEventListener('click', closeDetails);
 }
+
